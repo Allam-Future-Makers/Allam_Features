ac09c7f542675cd87b22256bc96f86f32f6d4bc4
Omar Khalid Allam <91850794+OmarAllam22@users.noreply.github.com>
2024-09-29 18:13:52 +0000

    change 'asnwer' to 'answer'

--- to_MSA/prompts.py
+++ to_MSA/prompts.py
@@ -36,7 +36,7 @@ correct_parser_prompt = PromptTemplate(template="""
 وقد تكون الجملة المصححة هى نفس الجملة الأصلية  
 give your answer as JSON with keys as 'input_sentence' and 'primary_corrected_sentence'
 هذه هى الإجابة المعطاة لك:
-{asnwer}
+{answer}
 """,
 input_variables=['answer'])
 
