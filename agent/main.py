30f468b13c62dd75f2fa295ef37decfd2685d3ee
omarallam22 <omar22allam@gmail.com>
2024-10-11 15:37:02 +0000

    updating introductory message

--- agent/main.py
+++ agent/main.py
@@ -7,10 +7,10 @@ warnings.filterwarnings("ignore", message="The function `loads` is in beta")
 from termcolor import colored
 from main_agent import ReActAgent
 
-print(colored("Welcome to our medical assistant.😊",'green')) # Introductory message to the user
+print(colored("Welcome to AQSA (Arabic Quality and Skills Accelerator).😊",'green')) # Introductory message to the user
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="A medical assistant using ReActLoop.")
+    parser = argparse.ArgumentParser(description="An application using ReActLoop.")
     parser.add_argument('--verbose', action=argparse.BooleanOptionalAction, required=True) # run the script as (python main.py --verbose  / python main.py --no-verbose)
     args = parser.parse_args()
     
