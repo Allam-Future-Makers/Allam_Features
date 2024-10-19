1aa6b67bc171b1d75f221070244a89290f20f697
HalemoGPA <haleemborham3@gmail.com>
2024-10-19 16:04:22 +0300

    update nothing

--- Ayah_Search/main.py
+++ Ayah_Search/main.py
@@ -1,6 +1,8 @@
 from scripts.run_search import search_quran
 
 
-query = "ادخلوها آمنين"
+query = "للذين استجابوا لربهم الحسنى"
 result, hits, best_hit = search_quran(query)
 # print(best_hit)
+# for hit in hits:
+#     print(hit["_source"]["Ayah"])
