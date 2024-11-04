3c2da90e0a59e18e580971901164fcd1381d12b5
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-11-04 19:26:36 +0200

    merge omar's edit

--- HolyQuran/chain.py
+++ HolyQuran/chain.py
@@ -1,5 +1,6 @@
-import sys,os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from HolyQuran.scripts.run_search import search_quran
 from HolyQuran.scripts.run_indexing import index_quran
@@ -9,9 +10,10 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_ibm import WatsonxLLM
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+
 class HolyQuranChain:
     def __init__(self, instance):
-        index_quran()   # created indices are stored here: sudo ls /var/lib/elasticsearch/indices/
+        index_quran()  # created indices are stored here: sudo ls /var/lib/elasticsearch/indices/
         self.instance = instance
 
         # Initialize models
@@ -21,55 +23,61 @@ class HolyQuranChain:
             "temperature": 0.00,
         }
 
-    
-    def _get_similar_context(query):
-        result, hits, best_hit = search_quran(query)
+    def _get_similar_context(self, query):
+        res, hits, best_hit = search_quran(query)
         context = ""
-        for i,hit in enumerate(1, hits[:3]):
+        for i, hit in enumerate(hits[:3], start=1):
             d = hit["_source"]
             c = {}
-            c['Ayah'] = d['I3rab'] 
-            c['Tafseer_Saadi'] = d['Tafseer_Saadi']
-            c['telawa'] = d['telawa']
+            c["الآية"] = d["I3rab"]
+            c["التفسير"] = d["Tafseer_Saadi"]
+            c["التلاوة"] = d["telawa"]
             context = context + "النص_{i}" + str(c)
         return context
-    
+
     def __call__(self, query):
 
         self.watsonx_llm = WatsonxLLM(
-            project_id= self.instance.watsons['project_id'],
-            apikey= self.instance.watsons['key'],
+            project_id=self.instance.watsons["project_id"],
+            apikey=self.instance.watsons["key"],
             model_id="sdaia/allam-1-13b-instruct",
             url="https://eu-de.ml.cloud.ibm.com",
-            params=self.model_params
+            params=self.model_params,
         )
 
         self.gemini_llm = ChatGoogleGenerativeAI(
-            model="gemini-1.5-flash", 
-            temperature=0, 
-            api_key=self.instance.gemini_keys[self.instance.iterator% len(self.instance.gemini_keys)]
+            model="gemini-1.5-flash",
+            temperature=0,
+            api_key=self.instance.gemini_keys[
+                self.instance.iterator % len(self.instance.gemini_keys)
+            ],
         )
 
-        
         try:
-            chain =(
-                {"query": RunnablePassthrough(),"context": RunnableLambda(self._get_similar_context)}
+            chain = (
+                {
+                    "query": RunnablePassthrough(),
+                    "context": RunnableLambda(self._get_similar_context),
+                }
                 | ayah_prompt_allam
                 | self.watsonx_llm
                 | JsonOutputParser()
             )
-            result = chain.invoke(query)['answer']
+            result = chain.invoke(query)["answer"]
         except:
             try:
-                chain =(
-                    {"query": RunnablePassthrough(),"context": RunnableLambda(self.get_similar_context)}
+                chain = (
+                    {
+                        "query": RunnablePassthrough(),
+                        "context": RunnableLambda(self._get_similar_context),
+                    }
                     | ayah_prompt_gemini
                     | self.gemini_llm
                     | JsonOutputParser()
                 )
-                result = chain.invoke(query)['answer']
+                result = chain.invoke(query)["answer"]
             except Exception as e:
                 print(f"Error has occured:{e}")
-        
+
         self.instance.iterator += 1
         return result
