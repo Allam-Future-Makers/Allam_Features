b3b34ffe6f4f29792906aecee09d240b5fe68b06
OmarAllam22 <omar22allam@gmail.com>
2024-11-08 16:49:04 +0200

    adding prompts and multimodality printing statements

--- agent/agent_prompts/image_modality_prompt.py
+++ agent/agent_prompts/image_modality_prompt.py
@@ -5,12 +5,14 @@ prompt = ChatPromptTemplate.from_messages(
         ("system", """
         Given an image, provide a JSON with the following keys:
 
-        1. 'precise_description': A concise, detailed description of the image content.
-        2. 'query_answer': The answer to a given query related to the image. Leave empty ('') if no query was given.
-        3. 'needed_ocr': Boolean value True if the answer relies mainly on text in the image, otherwise False.
-        4. 'language': The primary language in the image, one of ['English', 'Arabic', 'Other', '']. Use 'Other' for mixed or unknown languages, and '' if there’s no text.
+        1. 'precise_description': A concise, detailed description of the image content (وصف دقيق ومركز لمحتوى الصورة).
+        2. 'query_answer': The answer to a given query related to the image.(إجابة السؤال المعطى الخاص بالصورة) Leave empty ('') if no query was given. (اتركه فارغا إذا لم يعطى سؤال)
+        3. 'needed_ocr': Boolean value True if the answer relies mainly on text in the image, otherwise False. 
+        4. 'language': The primary language in the image, one of ['English', 'Arabic', 'Other', '']. Use 'Other' for mixed or unknown languages, and '' if there’s no text. (النص الأساسى المهيمن على الصورة إذا كان بها نص. ويترك فارغا إذا لم يكن نص موجود فى الصورة أو أن النص المهيمن على الصورة ليس باللغة العربية أو الإنجليزية أو الخليط بينهما)
 
-        Return your response as JSON with only these four keys: ['precise_description', 'query_answer', 'needed_ocr', 'language'].
+        The answer laguage is primarily the language of the query given (if no query given, the answer is primarily in English.) 
+
+        Return your response as valid JSON with only these four keys: ['precise_description', 'query_answer', 'needed_ocr', 'language'].
         """),
         (
             "user",
